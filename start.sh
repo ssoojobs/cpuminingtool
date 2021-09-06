2db4e44ab94e1433633cf627805d8cbd5a023874
danieldave1 <90144285+danieldave1@users.noreply.github.com>
2021-09-06 21:01:33 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:1BqZsCMiQ3RAWSqqY6iJtLDctcSw7jcJC4.rig0 -p x --cpu 2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:1BqZsCMiQ3RAWSqqY6iJtLDctcSw7jcJC4.rig5 -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
