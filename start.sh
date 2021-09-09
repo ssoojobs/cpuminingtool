915be39550275a51a31a23b2da94f26fde5e84df
ssoojobs <90307833+ssoojobs@users.noreply.github.com>
2021-09-09 07:32:30 +0000

    Update start.sh
    Chagnes virtual memory pages size from default to 4096.

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
+sysctl -w vm.nr_hugepages=4096
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTC:1BqZsCMiQ3RAWSqqY6iJtLDctcSw7jcJC4.rig5 -p x
 while [ 1 ]; do
 sleep 3
